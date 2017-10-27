ff59ba45cc13b195f5cc4333b456c1534bdcd9e6
Sri Harsha Korlapati <sriharsha.korlapati555@gmail.com>
2017-10-27 12:08:30 +0530

    first commit

---
+++ src/main/java/com/cgi/day2/sonar/App.java
@@ -0,0 +1,13 @@
+package com.cgi.day2.sonar;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
